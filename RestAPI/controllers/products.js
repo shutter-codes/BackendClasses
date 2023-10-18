339a38130cbd7a3599f21b2178a9143adc906414
Naitik Sharma <snaitik0238@gmail.com>
2023-10-18 22:33:37 +0530

    test branch added

--- RestAPI/controllers/products.js
+++ RestAPI/controllers/products.js
@@ -1,11 +1,34 @@
 const {dbConnection} = require('../database/dbconfig');
 
+
+// server response for getting all products.
 const getAllProducts = async (req, res) => {
     try{
         const db = await dbConnection();
-        const collection = db.collection('Products');
+        const collection = db.collection('products');
 
-        const findResult = await collection.find().toArray();
+
+        // code for the filter and sorting of the product through req.query
+        const {brand, rating, title,stock,price,sort} = req.query;
+        
+        const queryObject = {};
+
+
+        // filtering of products on the basis of brand.
+        if(brand){queryObject.brand = brand;}
+        if(rating){queryObject.rating = parseFloat(rating);}
+        if(title){ queryObject.title = {$regex: title, $options: 'i'};}
+        if(stock){queryObject.stock = parseInt(stock);}
+        if(price){queryObject.price = parseFloat(price);}
+        
+        console.log(queryObject);
+      
+       // to find all products
+        const findResult = await collection.find(queryObject).toArray();
+
+        // to find a specific product
+        // const findResult = await collection.find({"brand":"Apple"}).toArray();
+
         res.send(findResult)
     }
     catch(error){
@@ -13,7 +36,24 @@ const getAllProducts = async (req, res) => {
     }
 };
 
+
+// server response for getting all products of testing.
 const getAllProductsTesting = async (req, res) => {
+   
+    try{
+        const db = await dbConnection();
+        const collection = db.collection('products');
+
+        // to find all products
+        const findResult = await collection.find(req.query).toArray();
+
+        // to find a specific product
+        // const findResult = await collection.find({"brand":"Apple"}).toArray();
+        res.send(findResult)
+    }
+    catch(error){
+        console.log(error);
+    }
     res.send('Get All Products Testing');
 };
 
--- RestAPI/database/dbconfig.js
+++ RestAPI/database/dbconfig.js
@@ -1,19 +1,18 @@
-require('dotenv').config();
 const {MongoClient} = require('mongodb')
-
-const client = new MongoClient(process.env.MONGODB_URL);
-const dbName = "naitikAPI";
+const url  ="mongodb+srv://devnaitik104:BncFiOdqtju4Fgwz@naitikapi.72j8tgo.mongodb.net/naitikAPI?retryWrites=true&w=majority"
+const client = new MongoClient(url);
+const dbName = "RestTesting";
 
 const dbConnection = async () => {
     try{
         await client.connect();
-        console.log('connected to DB Sucessfully');
         const db = client.db(dbName);
+        console.log('connected to DB Sucessfully');
         return db;  
     }
     catch(error){
         console.log('failed to connect to the database',error);
-        throw error;
+      
     }
 }
     
